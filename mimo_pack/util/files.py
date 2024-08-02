7899df326334052458e41a13baea4c1c111db144
dbheadley <drewbheadley@gmail.com>
2024-08-02 15:30:24 -0400

    added support for dclut files

--- mimo_pack/util/files.py
+++ mimo_pack/util/files.py
@@ -29,3 +29,28 @@ def find_all_matching_files(directory, pattern):
                 matching_files.append(os.path.join(root, file))
     
     return matching_files
+
+def find_all_matching_directories(directory, pattern):
+    """
+    Find all directories in a directory and subdirectories that match a pattern.
+    
+    Parameters
+    ----------
+    directory : str
+        The directory to search.
+    pattern : str
+        The pattern to match as a regular expression
+        
+    Returns
+    -------
+    matching_dirs : list
+        A list of the directory paths that match the pattern.
+    """
+
+    matching_dirs = []
+    for root, dirs, files in os.walk(directory):
+        for dir in dirs:
+            if re.search(pattern, dir):
+                matching_dirs.append(os.path.join(root, dir))
+    
+    return matching_dirs
